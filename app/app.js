2b1ca99ee73f108cda93a1374f5e930b76f63282
magnushg <magnus.h.green@gmail.com>
2013-10-06 10:23:48 +0200

    jquery version of site

--- app/app.js
+++ app/app.js
@@ -10,35 +10,44 @@ function initialize() {
       }
 
 $(function () {
-	var viewModel = {entries : ko.observable(), url: ko.observable('http://hotell.difi.no/api/json/bergen/dokart?')};
+	var viewModel = {entries : [], url: 'http://hotell.difi.no/api/json/bergen/dokart?'};
 	
-	$.getJSON(viewModel.url(), function (data) {
-		viewModel.entries(_.map(data.entries, function (entry) {
+	$.getJSON(viewModel.url, function (data) {
+		viewModel.entries = _.map(data.entries, function (entry) {
 			return {
 				adresse: entry.adresse,
 				plassering: entry.plassering,
 				lat: entry.latitude,
 				lon: entry.longitude
 			}
-		}))
+		});
 
-		_.each(viewModel.entries(), function (entry) {
-			var marker = new google.maps.Marker({
-    		position: new google.maps.LatLng(entry.lat,entry.lon),
-    		map: map,
-    		title:entry.plassering
-    		});
+		  
+	var heading = '<h3>' + viewModel.url + '</h3>'
+	var tableHtml = '<table class="table table-responsive"><thead><th>Adresse</th><th>Plassering</th><th>Latitude</th><th>Longitude</th></thead><tbody>'
+    _.each(viewModel.entries, function (entry) {
+    	tableHtml += '<tr><td>' + entry.adresse + '</td><td>' + entry.plassering + '</td><td>' + entry.lat + '</td><td>' + entry.lon + '</td></tr>'
+    });
+    tableHtml += '</tbody></table>'
+    html = heading + tableHtml;
+    $("#dataTable").append(html);
 
-			var infowindow = new google.maps.InfoWindow({
-      			content: entry.plassering + ' ' + entry.adresse
-  			});
+	_.each(viewModel.entries, function (entry) {
+		var marker = new google.maps.Marker({
+		position: new google.maps.LatLng(entry.lat,entry.lon),
+		map: map,
+		title:entry.plassering
+		});
 
-  			 google.maps.event.addListener(marker, 'click', function() {
-    			infowindow.open(map,marker);
-  			});
-		})
+		var infowindow = new google.maps.InfoWindow({
+  			content: entry.plassering + ' ' + entry.adresse
+			});
+
+			 google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+			});
 	})
+})
 	initialize();
-	ko.applyBindings(viewModel);
 });
 
